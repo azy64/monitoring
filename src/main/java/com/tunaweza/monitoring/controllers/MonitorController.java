df80c7d958b5e74099520811800906559047e8a2
azaria <allysaidi64@gmail.com>
2025-02-14 16:04:25 +0100

    Add some changes

--- src/main/java/com/tunaweza/monitoring/controllers/MonitorController.java
+++ src/main/java/com/tunaweza/monitoring/controllers/MonitorController.java
@@ -29,7 +29,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
-@RequestMapping("/monitoring")
+@RequestMapping("/auth")
 public class MonitorController {
     
     private QrCodeInterface qrCodeService;
